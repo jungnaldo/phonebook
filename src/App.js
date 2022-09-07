b576714002ff637fd002e6d550ce5cd1908f1828
jungnaldo <jjs921203@gmail.com>
2022-09-07 15:44:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from "react-bootstrap";
+import "./App.css";
+import ContactForm from "./component/ContactForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ContactList from "./component/ContactList";
 
+///왼쪽에는 연락처 등록 폼이 , 오른쪽에는 연락처 리스트와 서치창이 있다.
+///유저가 이름을 입력하고 폰넘버 입력하면 리스트에 추가할수 있다.
+///리스트에 아이템이 몇개가 담기는지 보인다.
+///사용자가 유저를 이름으로 검색하여 찾을 수 있다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">연락처</h1>
+      {/* 모든것을 중앙에 위치시는 컨테이너 태그 */}
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col>
+            <ContactList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  ///Provider로 왜 감싸야 할까? Store를 사용하기 위해서 !
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
